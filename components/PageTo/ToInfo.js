ca6eccb3e977ed1409cd34ab93d805906b4c7e49
Phil <qft.gtr@gmail.com>
2022-12-08 15:37:59 -0800

    MesonToEmbedded

--- components/PageTo/ToInfo.js
+++ components/PageTo/ToInfo.js
@@ -1,35 +1,35 @@
 import React from 'react'
-import presets from '@mesonfi/presets'
+import dynamic from 'next/dynamic'
+
 import Jazzicon, { jsNumberForAddress } from 'react-jazzicon'
 import { QRCodeSVG } from 'qrcode.react'
-
 import Card from 'components/common/Card'
-import TokenSelector from 'components/common/TokenSelector'
+
+const MesonToEmbedded = dynamic(
+  import('@mesonfi/to').then(t => t.MesonToEmbedded),
+  { ssr: false }
+)
 
 export default function ToInfo ({ to }) {
   const uid = to.uid || to.address
 
   return (
-    <Card className='mt-4 mb-12 w-[428px] items-center'>
+    <Card className='mt-4 mb-12 w-[428px] px-4 items-center'>
       <div className='self-center w-16 h-16 rounded-full'>
         <Jazzicon seed={jsNumberForAddress(to.address)} diameter={64} />
       </div>
       
-      <div className='mt-2 font-light text-xs text-gray-800'>{to.address}</div>
-
-      <div className='mt-4 flex flex-col items-center'>
+      <div className='my-4 flex flex-col items-center'>
         <div className='font-bold text-xl'>{to.name}</div>
         <div className='font-light text-sm text-gray-800'>{to.desc}</div>
       </div>
 
-      <div className='mt-4 flex flex-col items-center'>
-        <div>{presets.getNetwork(to.networkId).name}</div>
-        <div className='mt-3 flex flex-row gap-3'>
-          {to.tokens.map(symbol => <TokenSelector key={`token-${symbol}`} selected symbol={symbol} />)}
-        </div>
-      </div>
-
-      <div className='mt-8 p-2 bg-white rounded-lg'>
+      <MesonToEmbedded
+        appId='alls-to'
+        to={to}
+        onCompleted={() => {}}
+      />
+      <div className='mt-4 p-2 bg-white rounded-lg'>
         <QRCodeSVG size={108} value={`https://alls.to/${to.uid}`} />
       </div>
     </Card>
--- components/PageTo/index.js
+++ components/PageTo/index.js
@@ -1,23 +1,15 @@
 import React from 'react'
 import { useRouter } from 'next/router'
-import dynamic from 'next/dynamic'
 
-import { useExtensions } from '@mesonfi/extensions/react'
-
-import { abbreviate } from 'lib'
 import Container from 'components/common/Container'
 import Header from 'components/common/Header'
 
 import bg from 'components/common/bg.jpg'
 
-import ConnectWallet from './ConnectWallet'
 import ToInfo from './ToInfo'
-
-const MesonToButton = dynamic(import('@mesonfi/to'), { ssr: false })
 
 export default function PageTo ({ to }) {
   const router = useRouter()
-  const { extensions, browserExt } = useExtensions()
 
   React.useEffect(() => {
     if (!to) {
@@ -29,29 +21,8 @@ export default function PageTo ({ to }) {
 
   return (
     <Container bg={bg}>
-      <Header logoSrc='https://alls.to'>
-        {
-          browserExt
-            ? <img alt={browserExt.name} crossOrigin='anonymous' className='w-6 h-6' src={browserExt.ext.icon} />
-            : <div className='w-6 h-6' />
-        }
-        <div className='font-bold'>{abbreviate(browserExt?.currentAccount.address)}</div>
-        <button
-          type='button'
-          className='items-center rounded-xl w-10 h-10 text-sm font-medium text-black bg-white hover:bg-gray-100 focus:outline-none shadow-md font-light text-lg'
-        >
-          x
-        </button>
-      </Header>
+      <Header logoSrc='https://alls.to' />
       <ToInfo to={to} />
-      {/* <MesonToButton
-        appId='alls-to'
-        type='iframe'
-        to={to}
-        onCompleted={() => {}}
-      >
-        Send Now
-      </MesonToButton> */}
     </Container>
   )
 }
