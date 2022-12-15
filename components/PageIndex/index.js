652e910f5051650303edec3d87be38b67961c9d1
Phil <qft.gtr@gmail.com>
2022-12-16 05:58:10 +0900

    show not installed exts

--- components/PageIndex/index.js
+++ components/PageIndex/index.js
@@ -2,6 +2,8 @@ import React from 'react'
 import classnames from 'classnames'
 import { useRouter } from 'next/router'
 import Image from 'next/image'
+import groupBy from 'lodash/groupBy'
+import mapValues from 'lodash/mapValues'
 
 import { useExtensions } from '@mesonfi/extensions/react'
 import { useWeb3Login } from '@mesonfi/web3-jwt/react'
@@ -73,21 +75,38 @@ function LoginWallets ({ loading, extensions, login }) {
 
   React.useEffect(() => {
     setTimeout(() => {
-      const exts = extensions.detectAllExtensions().filter(ext => !ext.notInstalled && ext.type !== 'walletconnect')
+      const exts = Object.values(mapValues(
+        groupBy(
+          extensions.detectAllExtensions().filter(ext => ext.type !== 'walletconnect'),
+          'type'
+        ),
+        grouped => grouped.every(ext => ext.notInstalled) ? grouped[0] : grouped.filter(ext => !ext.notInstalled)
+      )).flat()
       setExtList(exts)
     }, 100)
   }, [extensions])
 
   if (loading) {
-    return 'loading...'
+    return <div className='self-center font-light'>Loading...</div>
   }
 
   return (
-    <div className='flex flex-col gap-4'>
+    <div className='flex flex-col gap-4 -mx-8 px-8 max-h-[320px] overflow-y-auto'>
       {extList.map(ext => (
-        <Button key={ext.id} className='justify-between' onClick={() => login(ext)}>
-          <div>{ext.name}</div>
-          <div className='flex w-8 h-8 bg-white items-center justify-center rounded-md'>
+        <Button
+          key={ext.id}
+          className='justify-between'
+          onClick={() => ext.notInstalled ? window.open(ext.installLink, '_blank') : login(ext)}
+        >
+          {
+            ext.notInstalled
+            ? <div className='opacity-50'>Get {ext.name}</div>
+            : <div>{ext.name}</div>
+          }
+          <div className={classnames(
+            'flex w-8 h-8 bg-white items-center justify-center rounded-md',
+            ext.notInstalled && 'opacity-50'
+          )}>
             <img alt={ext.name} crossOrigin='anonymous' className='w-6 h-6' src={ext.icon} />
           </div>
         </Button>
