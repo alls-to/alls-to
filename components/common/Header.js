103fc5d0d699b0728d766b5795f88806db957e72
Phil <qft.gtr@gmail.com>
2022-12-30 21:15:19 +0800

    Add logo

--- components/common/Header.js
+++ components/common/Header.js
@@ -1,17 +1,21 @@
 import React from 'react'
 import classnames from 'classnames'
+import Image from 'next/image'
+
+import logo from './logo.svg'
 
 export default function Header ({ logoSrc, children }) {
   return (
     <div className='relative z-20 w-full flex flex-row items-center justify-between mt-5 px-6 xs:pl-8 xs:pr-9 h-10 shrink-0'>
       <a
         className={classnames(
-          'flex bg-gray-400 text-white h-[30px] w-[120px] font-light items-center justify-center rounded-lg',
+          'flex h-[30px] w-[160px] items-center',
           logoSrc && 'cursor-pointer'
         )}
         href={logoSrc}
       >
-        ALLS.TO LOGO
+        <Image alt='logo' width={28} height={28} src={logo} />
+        <div className='ml-2 text-[20px] font-light'>ALLsTo</div>
       </a>
       <div className='flex flex-row items-center gap-2.5'>
         {children}
