ca0aeeb105e9b7de8fb67ae924c48e58a5744f31
Phil <qft.gtr@gmail.com>
2023-01-06 10:50:24 +0800

    metadata preview

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,8 @@ import PageIndex from 'components/PageIndex'
 export default function Index() {
   const metadata = {
     title: 'ALLsTo',
-    description: process.env.METADATA_DESC || ''
+    description: process.env.METADATA_DESC || '',
+    previewImg: 'https://alls.to/preview.png'
   }
 
   const seo = <NextSeo
@@ -14,9 +15,9 @@ export default function Index() {
     openGraph={{
       title: metadata.title,
       description: metadata.description,
-      // images: [
-      //   { url: metadata.previewImg }
-      // ]
+      images: [
+        { url: metadata.previewImg }
+      ]
     }}
   />
   
