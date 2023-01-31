e184d3411b4b3f7e13a306a4dc58ddc2bce08eb7
Phil <qft.gtr@gmail.com>
2023-01-31 10:41:11 +0800

    Update index.js

--- pages/api/v1/did/index.js
+++ pages/api/v1/did/index.js
@@ -14,6 +14,7 @@ export default async function handler (req, res) {
     const profile = await getProfileFromAddress(addr)
     if (!profile) {
       res.status(400).end()
+      return
     }
     const doc = await Recipients.findByIdAndUpdate(addr, { did: 'cyberconnect', ...profile }, { new: true })
     res.json({ result: { address: doc._id, ...doc.toJSON(), ...profile } })
