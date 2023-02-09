5d1236d35297967b41ad5ed0d00524e57b94c4c9
Ekpe_ <20736452+iNorthIsle@users.noreply.github.com>
2023-02-09 13:38:49 +0800

    fix: avatar img size (#10)

--- components/PageTo/CardBody/Avatar/index.js
+++ components/PageTo/CardBody/Avatar/index.js
@@ -2,6 +2,6 @@ import Jazzicon, { jsNumberForAddress } from 'react-jazzicon'
 
 export default function Avatar ({ url, addr }) {
   return url
-    ? <img width='100%' height='100%' alt='' src={url} />
+    ? <img className='w-full h-full' alt='' src={url} />
     : <Jazzicon seed={jsNumberForAddress(addr)} diameter={64} />
 }
