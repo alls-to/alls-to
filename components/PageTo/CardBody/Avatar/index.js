b8bd784c0def529101010556b0d9a5b3beb5252c
Phil <qft.gtr@gmail.com>
2023-02-09 14:57:34 +0800

    fix Jazzicon for tron addr

--- components/PageTo/CardBody/Avatar/index.js
+++ components/PageTo/CardBody/Avatar/index.js
@@ -1,7 +1,11 @@
 import Jazzicon, { jsNumberForAddress } from 'react-jazzicon'
+import TronWeb from 'tronweb'
 
-export default function Avatar ({ url, addr }) {
+export default function Avatar ({ url, addr, diameter = 64 }) {
+  if (addr?.startsWith('T')) {
+    addr = TronWeb.address.toHex(addr).replace('41', '0x')
+  }
   return url
     ? <img className='w-full h-full' alt='' src={url} />
-    : <Jazzicon seed={jsNumberForAddress(addr)} diameter={64} />
+    : <Jazzicon seed={jsNumberForAddress(addr)} diameter={diameter} />
 }
