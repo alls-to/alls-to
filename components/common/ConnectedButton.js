d1a7219c87f777eefaeba085d04cde60d077aa3f
Phil <qft.gtr@gmail.com>
2023-02-13 00:00:14 +0800

    Update ConnectedButton.js

--- components/common/ConnectedButton.js
+++ components/common/ConnectedButton.js
@@ -10,7 +10,7 @@ export default function ConnectedButton ({ hideAddress, icon, addr = '', classNa
       <div className={classnames('w-5 h-5', !addr && 'filter-gray')}>
         <img alt='' crossOrigin='anonymous' className='w-5 h-5' src={icon} />
       </div>
-      <div className={classnames(hideAddress ? 'hidden' : 'ml-2 leading-5')}>
+      <div className={classnames(hideAddress || !addr ? 'hidden' : 'ml-2 leading-5')}>
         <div className='hidden sm:block'>{abbreviate(addr, 6)}</div>
         <div className='block sm:hidden'>{abbreviate(addr)}</div>
       </div>
