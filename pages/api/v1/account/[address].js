3c1ebc814935be9a69ce77d88b15be882154a498
Ekpe_ <20736452+iNorthIsle@users.noreply.github.com>
2023-02-21 17:06:24 +0800

    feat: add address query (#23)

---
+++ pages/api/v1/account/[address].js
@@ -0,0 +1,12 @@
+import { AllsTo } from 'lib/db'
+
+export default async function handler(req, res) {
+  const { address } = req.query
+
+  if (req.method === 'GET') {
+    const exist = await AllsTo.findOne({ addr: address.toLowerCase() })
+    res.json({ result: !!exist })
+  } else {
+    res.end()
+  }
+}
