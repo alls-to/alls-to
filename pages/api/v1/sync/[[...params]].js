9cd05213bf7b8e761199af031542353fe198ea2e
Ekpe_ <20736452+iNorthIsle@users.noreply.github.com>
2023-02-22 17:18:08 +0800

    fix: link3 query schema (#24)

--- pages/api/v1/sync/[[...params]].js
+++ pages/api/v1/sync/[[...params]].js
@@ -19,8 +19,14 @@ export default async function handler (req, res) {
     }
 
     const profile = await getProfileFromAddress(addr, did)
-    if (!profile) {
+    if (profile === false) {
       res.status(400).end()
+      return
+    }
+    
+    // if not found will return undefined
+    if (!profile) {
+      res.status(404).end()
       return
     }
     const key = req.query.params?.[0] || { $exists: false }
