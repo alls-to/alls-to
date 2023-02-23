884cac8fbf2475cf6b04c1bc3d2de0e18630b156
Ekpe_ <20736452+iNorthIsle@users.noreply.github.com>
2023-02-23 17:37:06 +0800

    feat: update edit and mypage layout (#29)

--- components/PageTo/CardTransfer.js
+++ components/PageTo/CardTransfer.js
@@ -1,5 +1,4 @@
 import React from 'react'
-
 import { useExtensions } from '@mesonfi/extensions/react'
 
 import CentralCardWithSideInfo from 'components/common/Card/CentralCardWithSideInfo'
@@ -10,6 +9,9 @@ import Button from 'components/common/Button'
 import ShareButton from './ShareButton'
 import CardBodyTransfer from './CardBody/CardBodyTransfer'
 import CardBodyEdit from './CardBody/CardBodyEdit'
+import Icon from 'components/icons'
+import { abbreviate } from 'lib'
+import classnames from 'classnames'
 
 export default function CardTransfer ({ to: initialTo, matchExt }) {
   const { extensions } = useExtensions()
@@ -56,17 +58,27 @@ export default function CardTransfer ({ to: initialTo, matchExt }) {
 
   return (
     <CentralCardWithSideInfo>
-      <Card bg='pos2' className='p-3 xs:p-4 md:p-6 text-primary'>
-        <div className='flex flex-row justify-between px-1 xs:px-0'>
+      <div className='relative'>
+        <div className='relative'>
           <CardTransferTitle
             isOwner={!!matchExt}
+            ext={matchExt}
             editing={editing}
+            addr={to.addr}
             onUpdate={onUpdateEditing}
           />
-          {!editing && <ShareButton to={to} />}
         </div>
-        {body}
-      </Card>
+        <Card bg='pos2' className='p-3 xs:p-4 md:p-6 text-primary'>
+          <div className='flex flex-row justify-between px-1 xs:px-0'>
+            {!editing &&
+              <>
+                <div className='font-semibold'>Transfer To</div>
+                <ShareButton to={to} />
+              </>}
+          </div>
+          {body}
+        </Card>
+      </div>
       <IntroRegion
         networkId={to.networkId}
         token={to.tokens[0]}
@@ -87,27 +99,48 @@ export default function CardTransfer ({ to: initialTo, matchExt }) {
   )
 }
 
-function CardTransferTitle ({ isOwner, editing, onUpdate }) {
+function CardTransferTitle ({ ext, addr, isOwner, editing, onUpdate }) {
+  const [walletIcon, setWalletIcon] = React.useState('')
+  const { extensions } = useExtensions()
+
+  React.useEffect(() => {
+    const exts = extensions.detectAllExtensions().filter(ext => ext.type !== 'walletconnect')
+    const icon = exts.find(item => item.id === ext)?.icon
+    setWalletIcon(icon)
+  }, [ext])
+
   if (isOwner) {
     return (
-      <div className='-my-1.5 -ml-2 flex p-1 rounded-xl bg-glass-200 gap-1'>
-        <Button
-          size='2xs'
-          type={editing ? 'transparent' : 'primary'}
-          onClick={() => onUpdate(false)}
-        >
-          My Page
-        </Button>
-        <Button
-          size='2xs'
-          type={editing ? 'primary' : 'transparent'}
-          onClick={() => onUpdate(true)}
-        >
-          Edit
-        </Button>
+      <div className={classnames('relative z-10 flex text-primary items-center mb-2', !editing && 'justify-between')}>
+        {
+          editing ? (
+            <div className='font-semibold'><span className='cursor-pointer inline-block w-7 h-7 rounded-full p-[7px] mr-3 bg-primary/20 hover:bg-primary/30' onClick={() => onUpdate(false)}><Icon type='left-arrow' /></span>Edit My Page</div>
+          ) : (
+            <>
+              <div className='flex flex-col'>
+                <span className='text-base font-semibold' onClick={() => onUpdate(false)}>
+                  My Page
+                </span>
+                <div className='flex'>
+                  <span className='inline-block mr-1 box-sizing w-4 h-4 p-[2px] rounded-full bg-white'><img className='w-full h-full' src={walletIcon} /></span>
+                  <span className='text-sm'>{abbreviate(addr, 9)}</span>
+                </div>
+              </div>
+              <Button
+                size='xs'
+                type='primary'
+                className='text-white !py-0 !px-2'
+                onClick={() => onUpdate(true)}
+              >
+                <span className='h-4 w-4 mr-2'><Icon type='edit' /></span>
+                Edit
+              </Button>
+            </>
+          )
+        }
       </div>
     )
   } else {
-    return <div className='font-semibold'>TRANSFER TO</div>
+    return null
   }
 }
