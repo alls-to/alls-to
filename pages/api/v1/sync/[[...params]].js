f02f6915b850c76109714b3f6e16ec98325043b1
Ekpe_ <20736452+iNorthIsle@users.noreply.github.com>
2023-03-21 22:32:26 +0800

    fix: did key and handle

--- pages/api/v1/sync/[[...params]].js
+++ pages/api/v1/sync/[[...params]].js
@@ -29,12 +29,30 @@ export default async function handler (req, res) {
       res.status(404).end()
       return
     }
+
     const key = req.query.params?.[0] || { $exists: false }
-    const doc = await AllsTo.findOneAndUpdate({ addr, key }, {
-      key: profile.key,
-      did,
-      ...profile
-    }, { new: true })
+    let doc = await AllsTo.findOne({ addr, key: profile.key })
+  
+    if (doc) {
+      // If bonded with did, return the profile directly.
+      if (doc.did) {
+        res.json({ result: doc })
+        return
+      }
+
+      // If not bonded with did, update profile and bind did.
+      doc = await AllsTo.findOneAndUpdate({ addr, key: profile.key }, {
+        ...profile,
+        did,
+      }, { new: true })
+
+      res.json({ result: doc })
+      return
+    }
+
+    doc = await AllsTo.findOneAndUpdate({ addr, key }, profile, { new: true })
+
+    // update record if user has only one profile.
     if (!doc) {
       res.status(404).end()
       return
