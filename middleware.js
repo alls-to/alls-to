98a969a4bebf860fb0f36f711b29235824533a67
Ekpe_ <20736452+iNorthIsle@users.noreply.github.com>
2023-04-06 08:54:02 +0800

    feat: add particle distribution by qrcode

--- middleware.js
+++ middleware.js
@@ -4,6 +4,18 @@ const { NODE_ENV } = process.env
 
 export function middleware(req) {
   if (NODE_ENV === 'production' && req.headers.get('x-forwarded-proto') !== 'https') {
+    const hasCode = req.nextUrl.pathname.startsWith('/code/')
+    if (hasCode) {
+      const code = req.nextUrl.pathname.split('/')[2]
+  
+      const response = NextResponse.redirect(`https://${req.headers.get('host')}`)
+      response.cookies.set('code', code, {
+        httpOnly: true,
+        maxAge: 10 * 60,
+        sameSite: 'strict',
+      })
+      return response
+    }
     return NextResponse.redirect(`https://${req.headers.get('host')}${req.nextUrl.pathname}`)
   }
 }
